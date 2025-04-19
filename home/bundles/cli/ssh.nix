fbc5e00fda370e61ec69236403e67c81230ab327
Jack Kelly <jpk.lly@gmail.com>
2025-04-18 22:05:29 -0400

    Add ssh config

--- home/bundles/cli/ssh.nix
+++ home/bundles/cli/ssh.nix
@@ -1,5 +1,20 @@
 {
   programs.ssh = {
     enable = true;
+    matchBlocks = {
+      personalgit = {
+        hostname = "github.com";
+        user = "git";
+        identityFile = "~/.ssh/id_github_jack-kelly";
+        identitiesOnly = true;
+      };
+      workgit = {
+        hostname = "github.com";
+        user = "git";
+        identityFile = "~/.ssh/id_ed25519";
+        identitiesOnly = true;
+      };
+    };
+    includes = [ "~/.ssh/config.work" ];
   };
 }
