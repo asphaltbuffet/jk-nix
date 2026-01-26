32689d3037eec75299557d84190ba4af89e87ceb
Jack Kelly <jpk.lly@gmail.com>
2026-01-26 10:58:59 -0500

    autostart obsidian, discord, signal in wungus

--- home/wungus.nix
+++ home/wungus.nix
@@ -1,8 +1,34 @@
-{ ... }:
+{ pkgs, ... }:
 {
   imports = [
     ./global
     ./bundles/desktop
     ./bundles/work
   ];
+
+  xdg.configFile = {
+    "autostart/obsidian.desktop".text = ''
+      [Desktop Entry]
+      Type=Application
+      Name=Obsidian
+      Exec=${pkgs.obsidian}/bin/obsidian
+      X-GNOME-Autostart-enabled=true
+    '';
+
+    "autostart/discord.desktop".text = ''
+      [Desktop Entry]
+      Type=Application
+      Name=Discord
+      Exec=${pkgs.discord}/bin/discord
+      X-GNOME-Autostart-enabled=true
+    '';
+
+    "autostart/signal-desktop.desktop".text = ''
+      [Desktop Entry]
+      Type=Application
+      Name=Signal
+      Exec=${pkgs.signal-desktop}/bin/signal-desktop
+      X-GNOME-Autostart-enabled=true
+    '';
+  };
 }
