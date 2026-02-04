e7a388518c5ee262edc2ce20f9a59c832e89a377
Jack Kelly <jpk.lly@gmail.com>
2026-02-04 16:46:17 -0500

    add work email config shenanigans for jj

--- home/bundles/cli/jujutsu.nix
+++ home/bundles/cli/jujutsu.nix
@@ -3,10 +3,17 @@
   programs.jujutsu = {
     enable = true;
     settings = {
-      user = {
-        name = "Jack Kelly";
-        email = "jpk.lly@gmail.com";
-      };
+      user.name = "Jack Kelly";
+      "--scope" = [
+        {
+          "--when".repositories = ["~/code/github.com/jack-kelly/"];
+          user.email = "jpk.lly@gmail.com";
+        }
+        {
+          "--when".repositories = ["~/code/github.com/utilidata/"];
+          user.email = "jkelly@utilidata.com";
+        }
+      ];
     };
   };
 }
