8edbd06d7c83e37da02408c6bf2d016c0d865736
Jack Kelly <jpk.lly@gmail.com>
2026-02-24 11:25:40 -0500

    add flashing tools to bungus

--- host/bungus/configuration.nix
+++ host/bungus/configuration.nix
@@ -1,3 +1,4 @@
+{ pkgs, ... }:
 {
   imports = [
     ../bundles/global
@@ -6,4 +7,28 @@
 
   networking.hostName = "bungus"; # Define your hostname.
   system.stateVersion = "24.05";
+
+  # Jetson Linux (L4T) flash dependencies
+  environment.systemPackages = with pkgs; [
+    abootimg
+    binutils
+    cpio
+    dtc
+    dosfstools
+    lbzip2
+    libxml2
+    lz4
+    netcat-openbsd
+    openssl
+    (python3.withPackages (p: [ p.pyyaml ]))
+    rsync
+    sshpass
+    whois
+    xmlstarlet
+    zstd
+  ];
+
+  services.nfs.server.enable = true;
+  boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
+  programs.nix-ld.enable = true;
 }
