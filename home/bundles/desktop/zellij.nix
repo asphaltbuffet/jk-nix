617ee853906bfb97393394363a5ea15771887b72
Jack Kelly <jpk.lly@gmail.com>
2026-04-09 12:26:55 -0400

    remove zellij startup tips

--- home/bundles/desktop/zellij.nix
+++ home/bundles/desktop/zellij.nix
@@ -4,6 +4,8 @@
     enableZshIntegration = true;
     settings = {
       copy_command = "xclip -selection clipboard";
+      ui.pane_frames.hide_session_name = true;
+      show_startup_tips = false;
     };
   };
 }
